371b5511a04efd4f8c8fc15f6996efc244f40df8
sam <sblan45@gmail.com>
2020-08-15 21:12:30 -0400

    creates frontend for simplelist

---
+++ src/views/QuotesIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="quotes-index">
+    <div class="container">
+      <div v-for="quote in quotes">
+        <h1>Title</h1>  
+        {{quote.context}}  
+        <h3>actor</h3> 
+        {{quote.source}} 
+        <h3>quote</h3> 
+        {{quote.quote}}
+        <h3>theme</h3>
+        {{quote.theme}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+// @ is an alias to /src
+
+export default {
+  data: function () {
+    return {
+      quotes: [],
+    };
+  },
+  created: function () {
+    fetch(
+      "https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => (this.quotes = data));
+  },
+};
+</script>
